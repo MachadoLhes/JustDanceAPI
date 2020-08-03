87697e9d05df83d0c717696528c30f7490ebc488
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2020-08-03 11:12:51 -0300

    deploy[heroku]: some changes

--- main/settings.py
+++ main/settings.py
@@ -1,5 +1,4 @@
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,6 +110,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
